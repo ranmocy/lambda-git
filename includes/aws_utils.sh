ca470985982611ec1c4b0cbef503e0818520f054
Ranmocy <ranmocy@gmail.com>
2022-10-05 08:04:25 -0400

    Exclude unavailable regions

--- includes/aws_utils.sh
+++ includes/aws_utils.sh
@@ -6,5 +6,5 @@ function list_aws_regions() {
     aws ssm get-parameters-by-path \
         --path /aws/service/global-infrastructure/services/lambda/regions \
         --query 'Parameters[].Value' \
-        --output text | tr '[:blank:]' '\n' | grep -v -e ^cn- -e ^us-gov- -e ^me- -e ^af- -e ^ap-east-1 -e ^eu-south-1 | sort -r
+        --output text | tr '[:blank:]' '\n' | grep -v -e ^cn- -e ^us-gov- -e ^me- -e ^af- -e ^ap-east-1 -e ^eu-south-1 -e ^ap-southeast-3 | sort -r
 }
